c45f57511958b793137ff74f5b80f3d73f629a0c
Daniel Wutke <dwu.public@gmail.com>
2024-01-31 20:26:11 +0100

    Add trace2csv converter

---
+++ js/trace2csv.js
@@ -0,0 +1,111 @@
+function createCsv(dataJson) {
+  const rows = new Array();
+
+  // index by id
+  for (const t of dataJson.items) {
+    spansById.set(t.id, t);
+  }
+
+  // link parent object
+  for (const [_, t] of spansById.entries()) {
+    t.parent = spansById.get(t.parentId);
+  }
+
+  // link child items
+  for (const [_, t] of spansById.entries()) {
+    t.children = new Array();
+    for (const [_, tc] of spansById.entries()) {
+      if (tc.parentId == t.id && tc.id != t.id) {
+        t.children.push(tc);
+      }
+    }
+    t.children = t.children.sort((a, b) => a - b);
+  };
+
+  // fill root level
+  let nextLevel = new Array();
+  let currentLevel = new Array();
+  for (const [_, t] of spansById.entries()) {
+    if (t.parent === undefined) {
+      currentLevel.push(t);
+    }
+  }
+
+  rows.push([
+    "id",
+    "level",
+    "parentid",
+    "numchildren",
+    "numerrors",
+    "name",
+    "timestamp",
+    "serviceid",
+    "servicelabel",
+    "endpointid",
+    "endpointlabel",
+    "endpointtype",
+    "duration",
+    "minSelfTime",
+    "networkTime",
+    "technologies"
+  ].join(";"));
+
+  // iterate levels
+  let level = 0;
+  for (;;) {
+    // iterate element of current level
+    for (let item of currentLevel) {
+      rows.push([
+        item.id,
+        level,
+        item.parentId,
+        item.children.length,
+        item.errorCount,
+        item.name,
+        item.timestamp,
+        item.destination.service.id,
+        item.destination.service.label,
+        item.destination.endpoint.id,
+        item.destination.endpoint.label,
+        item.destination.endpoint.type,
+        item.duration,
+        item.minSelfTime,
+        item.networkTime == null ? "" : item.networkTime,
+        item.destination.technologies.join("|")
+      ].join(";"));
+      nextLevel = nextLevel.concat(item.children);
+    }
+
+    if (nextLevel.length == 0) {
+      break;
+    }
+
+    currentLevel.length = 0;
+    currentLevel = currentLevel.concat(nextLevel);
+    nextLevel.length = 0;
+
+    level += 1;
+  }
+  return rows;
+}
+
+const spansById = new Map();
+
+const timelineContainer = document.getElementById("timeline");
+const fileSelector = document.getElementById("fileselector");
+const csvData = document.getElementById("csvdata");
+
+function readFile(file) {
+  const reader = new FileReader();
+  reader.addEventListener("load", (event) => {
+    const dataJson = JSON.parse(event.target.result);
+    csvData.innerHTML = createCsv(dataJson).join("\n");
+  });
+  reader.readAsText(file);
+}
+
+// file uploaded
+fileSelector.value = null;
+fileSelector.addEventListener("change", (event) => {
+  readFile(event.target.files[0]);
+});
